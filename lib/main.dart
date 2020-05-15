a37fa876806f85563b81442b743ab4deabe8d4cd
Stef-GMS <GeekMeSpeak@gmail.com>
2020-05-14 18:14:06 -0700

    add words, rename, push w/ desktop app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:testingrenamingobjects/testing_here/my_home_page.dart';
+import 'package:testingrenamingobjects/testing_here/my_home_page_rename3.dart';
 
 void main() {
   runApp(MyApp());

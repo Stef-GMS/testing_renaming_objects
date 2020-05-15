94ead64e3dac02c9c94123672024551978e4b240
Stef-GMS <GeekMeSpeak@gmail.com>
2020-05-14 18:11:50 -0700

    removed words and renamed in one git commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:testingrenamingobjects/testing_here/my_home_page_second_rename.dart';
+import 'package:testingrenamingobjects/testing_here/my_home_page.dart';
 
 void main() {
   runApp(MyApp());
